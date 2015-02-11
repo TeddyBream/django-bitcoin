13f19cc5ff5f0467887567b82a77ebe3272e2b49
Andrew Denisenko <nitr0@seti.kr.ua>
2015-02-11 19:56:07 +0200

    fix

--- django_bitcoin/tasks.py
+++ django_bitcoin/tasks.py
@@ -59,7 +59,7 @@ def query_transactions():
             query_block = settings.BITCOIN_START_BLOCK
             block = PersistIntCache.objects.create(key="queried_block_index",
                                                    value=query_block)
-        logger.info("Monitor transactions from block %d to %d") %
+        logger.info("Monitor transactions from block %d to %d" %
                     (query_block, blockcount))
         blockhash = bitcoind.bitcoind_api.getblockhash(query_block)
         transactions = bitcoind.bitcoind_api.listsinceblock(blockhash)
