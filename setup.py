210616b169fc95a4522c880454bbece623ebb6b5
jvn <jiivan@o2.pl>
2015-06-26 16:24:58 +0200

    fixed requirements

--- setup.py
+++ setup.py
@@ -17,14 +17,22 @@ packages = ['django_bitcoin',
 
 long_description = open("README.rst").read() + "\n" + open("CHANGES.rst").read()
 
+requirements = [
+    'celery',
+    'distributedlock',
+    'pytz',
+    #'qrcode>2.3.1',
+]
+
 setup(name='django-bitcoin',
       version='0.2.1',
       description='Bitcoin application integration for Django web framework',
       long_description=long_description,
       author='42 Coffee Cups',
       url='https://github.com/42cc/django-bitcoin',
-      requires=["qrcode (>2.3.1)", "South (>0.7.4)"],
+      #requires=["qrcode (>2.3.1)", "South (>0.7.4)"],
       license="MIT",
       packages=packages,
       package_data=dict((package_name, template_patterns) for package_name in packages),
+      install_requires=requirements,
       )
